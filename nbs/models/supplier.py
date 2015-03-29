43053b9faa92e16af9f5876aa40e8142913633fa
Augusto Roccasalva <augusto@rioplomo.com.ar>
2015-03-29 04:55:19 -0300

    Inherit Supplier from Entity, add models mixins.

--- nbs/models/supplier.py
+++ nbs/models/supplier.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from nbs.models import db
+from nbs.models.entity import Entity
+from nbs.models.misc import FiscalDataMixin
 
 
-class Supplier(db.Model):
+class Supplier(Entity, FiscalDataMixin):
     __tablename__ = 'supplier'
+    __mapper_args__ = {'polymorphic_identity': u'supplier'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode, nullable=False)
-    payment = db.Column(db.Integer)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('entity.id'),
+                            primary_key=True)
+    name = Entity._name_1
+    fancy_name = Entity._name_2
+
+    payment_term = db.Column(db.Integer)
 
     @property
-    def full_desc(self):
-        return "{0} {1}".format(self.name, self.payment)
+    def full_name(self):
+        fn = u" ({0})".format(self.fancy_name) if self.fancy_name else u""
+        return u"{0}{1}".format(self.name, fn)
