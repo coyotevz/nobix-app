f5e4c1c0429ca06365778db7668296c266335628
Augusto Roccasalva <augusto@rioplomo.com.ar>
2015-04-22 03:43:31 -0300

    Add some methods to employee api, delete needs more work

--- nbs/api/hr.py
+++ nbs/api/hr.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import jsonify, request, url_for
-from nbs.models import Employee, AttendanceRecord
+from nbs.models import db, Employee, AttendanceRecord
 from nbs.schema import EmployeeSchema, AttendanceRecordSchema
 from nbs.utils.api import ResourceApi, NestedApi, route, build_result
 from nbs.utils.args import get_args, build_args
@@ -18,3 +18,16 @@ class EmployeeApi(ResourceApi):
 
     def index(self):
         return build_result(Employee.query, employee_s)
+
+    @route('<int:id>')
+    def get(self, id):
+        employee = self.get_obj(id)
+        return build_result(employee, employee_s)
+
+    @route('<int:id>', methods=['DELETE'])
+    def delete(self, id):
+        employee = self.get_obj(id)
+        db.session.delete(employee)
+        db.session.commit()
+        # TODO: Remove related attendance records
+        return '', 204
