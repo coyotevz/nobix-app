e1bdd5a149fedcc98f568987c6c4284075201428
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-03-27 21:44:23 -0300

    Convert account_type in model

--- nbs/models/bank.py
+++ nbs/models/bank.py
@@ -23,27 +23,23 @@ class Bank(db.Model):
         return "<Bank '{}'>".format(self.name)
 
 
+class BankAccountType(db.Model):
+    __tablename__ = 'bank_account_type'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True, nullable=False)
+    abbr = db.Column(db.Unicode)
+
+
 class BankAccount(db.Model):
     __tablename__ = 'bank_account'
 
-    TYPE_CC_PESOS = 'TYPE_CC_PESOS'
-    TYPE_CC_USD = 'TYPE_CC_USD'
-    TYPE_CA_PESOS = 'TYPE_CA_PESOS'
-    TYPE_CA_USD = 'TYPE_CA_USD'
-    TYPE_UNIQUE = 'TYPE_UNIQUE'
-
-    _account_types = {
-        TYPE_CC_PESOS: 'Cuenta Corriente en Pesos',
-        TYPE_CC_USD: 'Cuenta Corriente en Dólares',
-        TYPE_CA_PESOS: 'Caja de Ahorro en Pesos',
-        TYPE_CA_USD: 'Caja de Ahorro en Dólares',
-        TYPE_UNIQUE: 'Cuenta Única',
-    }
-
     id = db.Column(db.Integer, primary_key=True)
     bank_branch = db.Column(db.Unicode)
-    type = db.Column(db.Enum(*_account_types.keys(),
-                     name='account_types'), default=TYPE_CC_PESOS)
+
+    type_id = db.Column(db.ForeignKey('bank_account_type.id'))
+    type = db.relationship(BankAccountType)
+
     number = db.Column(db.Unicode)
     cbu = db.Column(db.Unicode)
     owner = db.Column(db.Unicode)
@@ -58,12 +54,8 @@ class BankAccount(db.Model):
     def cbu_is_valid(self, key, cbu):
         assert validate_cbu(cbu)
 
-    @property
-    def account_type_str(self):
-        return self._account_types[self.account_type]
-
     def __repr__(self):
         return "<BankAccount '{}, {}: {}' of '{}'>".format(
-            self.bank.name, self.account_type_str, self.account_number,
+            self.bank.name, self.type.name, self.number,
             self.supplier.name
         )
