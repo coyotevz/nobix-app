940a2df4d0d3f0a133a0a818cafbd43aefac5081
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-03-27 21:21:02 -0300

    Add bcra_code, cuit to Bank model with cuit validator, add cbu validator to BankAccount

--- nbs/models/bank.py
+++ nbs/models/bank.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from sqlalchemy.orm import validates
+
 from nbs.models import db
+from nbs.utils.validators import validate_cuit, validate_cbu
 
 
 class Bank(db.Model):
@@ -8,7 +11,13 @@ class Bank(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode, unique=True, nullable=False)
+    bcra_code = db.Column(db.Unicode(8))
+    cuit = db.Column(db.Unicode(11))
     # TODO: Add bank logo, to quickly identify
+
+    @validates('cuit')
+    def cuit_is_valid(self, key, cuit):
+        assert validate_cuit(cuit)
 
     def __repr__(self):
         return "<Bank '{}'>".format(self.name)
@@ -33,17 +42,21 @@ class BankAccount(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     bank_branch = db.Column(db.Unicode)
-    account_type = db.Column(db.Enum(*_account_types.keys(),
-                             name='account_types'), default=TYPE_CC_PESOS)
-    account_number = db.Column(db.Unicode)
-    account_cbu = db.Column(db.Unicode)
-    account_owner = db.Column(db.Unicode)
+    type = db.Column(db.Enum(*_account_types.keys(),
+                     name='account_types'), default=TYPE_CC_PESOS)
+    number = db.Column(db.Unicode)
+    cbu = db.Column(db.Unicode)
+    owner = db.Column(db.Unicode)
 
     bank_id = db.Column(db.Integer, db.ForeignKey('bank.id'), nullable=False)
     bank = db.relationship(Bank, backref="accounts")
 
     supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.supplier_id'))
     supplier = db.relationship('Supplier', backref='bank_accounts')
+
+    @validates('cbu')
+    def cbu_is_valid(self, key, cbu):
+        assert validate_cbu(cbu)
 
     @property
     def account_type_str(self):
