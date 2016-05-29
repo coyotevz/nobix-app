ca6da588df02cec32a1afd91595126b6a4cca5ae
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-05-29 04:54:40 -0300

    Moved inherited files to attic folder

--- nbs/api/__init__.py
+++ nbs/api/__init__.py
@@ -2,14 +2,6 @@
 
 from flask import Blueprint, url_for
 
-#from nbs.api.user import UserApi
-#from nbs.api.supplier import SupplierApi
-#from nbs.api.hr import EmployeeApi
-#from nbs.api.bank_account import BankApi
-#from nbs.api.purchase_order import PurchaseOrderApi
-#from nbs.api.product import ProductApi
-
-from nbs.api.supplier import supplier_api
 from nbs.api.bank import bank_api
 
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -27,11 +19,4 @@ def documentation():
 
 def configure_api(app):
     app.register_blueprint(api)
-    app.register_blueprint(supplier_api)
     app.register_blueprint(bank_api)
-    #UserApi.register(app)
-    #SupplierApi.register(app)
-    #EmployeeApi.register(app)
-    #BankApi.register(app)
-    #PurchaseOrderApi.register(app)
-    #ProductApi.register(app)
--- nbs/models/__init__.py
+++ nbs/models/__init__.py
@@ -8,15 +8,6 @@ db = SQLAlchemy()
 def configure_db(app):
     db.init_app(app)
 
-
-from nbs.models.supplier import Supplier
-from nbs.models.contact import Contact, SupplierContact
-from nbs.models.hr import Employee, AttendanceRecord
-from nbs.models.fiscal import FiscalData
-from nbs.models.bank import Bank, BankAccount, BankAccountType
-from nbs.models.document import (
-    PurchaseDocument, PurchaseOrder, PurchaseOrderItem
-)
+from nbs.models.entity import Entity
 from nbs.models.misc import Address, Email, Phone, ExtraField
-
-from nbs.models.product import Product
+from nbs.models.bank import Bank, BankAccount, BankAccountType
